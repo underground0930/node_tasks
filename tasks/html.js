a6fd1d4bb9ea6ed84ed9c7129c73afb67faab046
underground0930 <go.nishiduka.1985@gmail.com>
2021-02-22 16:00:28 +0900

    ejs内のデータの持たせ方を変更

--- tasks/html.js
+++ tasks/html.js
@@ -21,30 +21,35 @@ const html = (src, dist, data) => {
     const { length } = files
     let count = 0
     files.forEach(file => {
-      ejs.renderFile(file, { data }, { outputFunctionName: 'echo' }, (err, str) => {
-        if (err) {
-          console.log(err)
-          return
-        }
-        const f = file.split(src)
-        const filename = dist + f[1]
-        const dir = path.dirname(filename)
-        if (!fs.existsSync(dir)) {
-          // ディレクトリが無かったら
-          fs.mkdirsSync(dir) // ディレクトリを再帰的に作成
-        }
-        fs.writeFile(filename, str, err => {
-          // ファイルに書き込む処理
-          if (err) throw err
-          resultArr.push(f[1])
-          count++
-          if (count === length) {
-            // ファイル数を数えてタスクが完了
-            console.log('ejs: [' + resultArr.join(', ') + ']')
-            console.log('====== ejs finished ======')
+      ejs.renderFile(
+        file,
+        { data, time: new Date().getTime() },
+        { outputFunctionName: 'echo' },
+        (err, str) => {
+          if (err) {
+            console.log(err)
+            return
           }
-        })
-      })
+          const f = file.split(src)
+          const filename = dist + f[1]
+          const dir = path.dirname(filename)
+          if (!fs.existsSync(dir)) {
+            // ディレクトリが無かったら
+            fs.mkdirsSync(dir) // ディレクトリを再帰的に作成
+          }
+          fs.writeFile(filename, str, err => {
+            // ファイルに書き込む処理
+            if (err) throw err
+            resultArr.push(f[1])
+            count++
+            if (count === length) {
+              // ファイル数を数えてタスクが完了
+              console.log('ejs: [' + resultArr.join(', ') + ']')
+              console.log('====== ejs finished ======')
+            }
+          })
+        }
+      )
     })
   })
 }
