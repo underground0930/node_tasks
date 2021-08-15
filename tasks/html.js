3ca1e8a4590ecbed7e6c8916aad21a135e47bd94
underground0930 <go.nishiduka.1985@gmail.com>
2021-08-15 19:51:32 +0900

    Beautifyの設定 preserve_newlinesを変更

--- tasks/html.js
+++ tasks/html.js
@@ -17,7 +17,7 @@ const beautify = require('js-beautify');
 const beautifyOptions = {
   indent_size: 2,
   end_with_newline: true,
-  preserve_newlines: false,
+  preserve_newlines: true,
   max_preserve_newlines: 0,
   wrap_line_length: 0,
   wrap_attributes_indent_size: 0,
