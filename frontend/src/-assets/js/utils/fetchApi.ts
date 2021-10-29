a89a880dfcb72cb96a1e6fb2c10c8f0a353a3ea3
underground0930 <go.nishiduka.1985@gmail.com>
2021-10-29 18:40:12 +0900

    axiosからfetchに変更

---
+++ frontend/src/-assets/js/utils/fetchApi.ts
@@ -0,0 +1,42 @@
+/**
+ * fetch APIの ラッパー
+ */
+
+type Props = {
+  url: string;
+  init: RequestInit;
+  debug?: boolean;
+};
+
+export function fetchApi<T>({
+  url,
+  init,
+  debug = false,
+}: Props): Promise<{ data: T | null; fetchError: string | null }> {
+  return fetch(url, init)
+    .then((response) => {
+      if (debug) {
+        console.log(
+          '[ok]: ' + response.ok,
+          '[status]: ' + response.status,
+          '[text]: ' + response.statusText
+        );
+      }
+      if (!response.ok || response.status === 0) {
+        throw new Error(response.statusText); // 0, 400, 500 errors
+      }
+      return response.json();
+    })
+    .then((data) => {
+      return {
+        data: data,
+        fetchError: null,
+      };
+    })
+    .catch((e) => {
+      return {
+        data: null,
+        fetchError: e.message,
+      };
+    });
+}
